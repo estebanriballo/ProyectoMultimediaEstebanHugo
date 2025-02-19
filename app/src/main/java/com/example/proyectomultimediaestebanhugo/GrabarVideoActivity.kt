685117f1748a00bc0a35c09aab2fa03272c06146
estebanriballo <estebanriballo@gmail.com>
2025-02-19 22:56:25 +0100

    Creacion Activities y acceso a las mismas desde la Activity principal

---
+++ app/src/main/java/com/example/proyectomultimediaestebanhugo/GrabarVideoActivity.kt
@@ -0,0 +1,20 @@
+package com.example.proyectomultimediaestebanhugo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GrabarVideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_grabar_video)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
